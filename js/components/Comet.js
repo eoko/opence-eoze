e957917c8204bde83656608807f8c617c55eb6fd
?ric Ort?ga <rixo@protonmail.ch>
2011-01-16 19:22:46 +0100

    Disabled experimental comet

--- js/components/Comet.js
+++ js/components/Comet.js
@@ -1,31 +1,31 @@
-Oce.deps.wait('Oce.Bootstrap.start', function() {
-
-	var userId;
-	if (Oce.mx.Security.isIdentified()) {
-		userId = Oce.mx.Security.getLoginInfos().userId;
-		poll();
-	}
-	Oce.mx.Security.addListener('login', function(info) {
-		userId = info.userId;
-		poll();
-	});
-
-	function poll() {
-		Ext.Ajax.request({
-			url: "comet.php"
-			,params: {
-				id: userId
-			}
-			,success: function(response) {
-				var o = Ext.decode(response.responseText);
-				if (o.alert) {
-					alert(o.alert);
-				}
-				poll();
-			}
-			,failure: function() {
-				poll()
-			}
-		});
-	}
-});
+//Oce.deps.wait('Oce.Bootstrap.start', function() {
+//
+//	var userId;
+//	if (Oce.mx.Security.isIdentified()) {
+//		userId = Oce.mx.Security.getLoginInfos().userId;
+//		poll();
+//	}
+//	Oce.mx.Security.addListener('login', function(info) {
+//		userId = info.userId;
+//		poll();
+//	});
+//
+//	function poll() {
+//		Ext.Ajax.request({
+//			url: "comet.php"
+//			,params: {
+//				id: userId
+//			}
+//			,success: function(response) {
+//				var o = Ext.decode(response.responseText);
+//				if (o.alert) {
+//					alert(o.alert);
+//				}
+//				poll();
+//			}
+//			,failure: function() {
+//				poll()
+//			}
+//		});
+//	}
+//});
