41024619df350578c69cfd481238720e03baf50b
?ric Ort?ga <rixo@protonmail.ch>
2011-02-21 23:45:00 +0100

    Adapter System for database connection (flexibility !)

---
+++ php/eoko/database/Adapter.class.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace eoko\database;
+
+use eoko\util\collection\Map;
+
+interface Adapter {
+
+	/** @return PDO */
+	function getConnection();
+
+	/**
+	 * Tests whether the given (sql) table matches the configured table prefix.
+	 * @param string $sqlTableName
+	 * @return boolean TRUE if the given $name is prefixed with the configured
+	 * table prefix or if no table prefix is configured, else FALSE.
+	 */
+	function testTablePrefix($dbTableName);
+
+	function internalTableNameToDatabase($internalTableName);
+
+	/**
+	 * Converts the name of a table as it appears in the datastore to the name
+	 * used internally by Cqlix (that is, the name minused from its prefix, if
+	 * one is configured).
+	 * @return string
+	 * @throws \InvalidArgumentException if the given $dbTableName is not a
+	 * valid name because its prefix doesn't match the configured one.
+	 */
+	function databaseTableNameToInternal($dbTableName);
+
+	/**
+	 * @return string
+	 */
+	function getDatabaseName();
+
+	/**
+	 * @return Map
+	 */
+	function getConfig();
+}
