cc9aa62845566aeb480de5fe9458f5804a25a1ff
?ric Ort?ga <rixo@protonmail.ch>
2011-02-22 00:07:41 +0100

    Refined Database utility class

--- php/eoko/database/Database.class.php
+++ php/eoko/database/Database.class.php
@@ -2,17 +2,36 @@
 
 namespace eoko\database;
 
+use eoko\config\ConfigManager;
+
 class Database {
 
 	private function __construct() {}
 
 	private static $defaultAdapter = null, $defaultConnection = null;
+	
+	private static $config = null;
+
+	/**
+	 * Get config for database from the default node.
+	 * @return eoko\util\collection\Map
+	 */
+	public static function getDefaultConfig() {
+		if (!self::$config) self::$config = ConfigManager::getConfigObject(__NAMESPACE__);
+		return self::$config;
+	}
 
 	/**
 	 * @return Adapter
 	 */
 	public static function getDefaultAdapter() {
-		if (!self::$defaultAdapter) self::$defaultAdapter = new Adapter\MysqlAdapter();
+		
+		if (!self::$defaultAdapter) {
+			$config = self::getDefaultConfig();
+			$class = __NAMESPACE__ . "\\Adapter\\{$config->adapter}Adapter";
+			self::$defaultAdapter = new $class($config);
+		}
+		
 		return self::$defaultAdapter;
 	}
 
