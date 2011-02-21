bced234e9e0bba27813f32d75c11547b3b147098
?ric Ort?ga <eric@planysphere.fr>
2011-02-21 23:45:00 +0100

    Adapter System for database connection (flexibility !)

---
+++ php/eoko/database/Adapter/MysqlAdapter.class.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace eoko\database\Adapter;
+
+use InvalidConfigurationException;
+use PDO;
+
+class MysqlAdapter extends AbstractAdapter {
+
+	protected function createConnection() {
+
+		$this->connection = new PDO(
+			"mysql:dbname={$this->config->database};host={$this->config->host}",
+			$this->config->user,
+			$this->config->password
+		);
+
+		$this->setNames();
+
+		return $this->connection;
+	}
+
+	private function setNames() {
+		$cs = $this->config->characterSet;
+		$collation = $this->config->collation;
+		if ($cs !== null) {
+			if ($collation) {
+				$this->connection->prepare("SET NAMES '$cs' COLLATE '$collation'")->execute();
+			} else {
+				throw new \InvalidConfigurationException(
+					$this->config,
+					"Both collation and characterSet must be set or left blank."
+				);
+			}
+		} else if ($collation !== null) {
+			throw new \InvalidConfigurationException(
+				$this->config,
+				"Both collation and characterSet must be set or left blank."
+			);
+		}
+	}
+
+}
