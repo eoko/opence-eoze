e0c8d3c2418bbadd0fa3e82c3b144a311e78c274
Merlin <romain.dary@eoko.fr>
2011-03-07 12:04:51 -0500

    new doc for singleton

--- php/eoko/util/Singleton.class.php
+++ php/eoko/util/Singleton.class.php
@@ -1,6 +1,35 @@
 <?php
+/**
+ *
+ * PHP versions 5.3
+ *
+ * LICENSE: This source file is subject to version 3.0 of the GPL license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/licenses/gpl.txt .  If you did not receive a copy of
+ * the GPL v3 License and are unable to obtain it through the web, please
+ * send a note to contact@eoko.fr so we can mail you a copy immediately.
+ * 
+ */
 
 namespace eoko\util;
+
+
+/**
+ *
+ * The singleton pattern is a design pattern used to implement the 
+ * concept of a singleton, by restricting the instantiation of a class to one 
+ * object.
+ * 
+ * @category   design pattern
+ * @package    util
+ * @author     Éric Ortéga <eric@eoko.fr>
+ * @author     Romain DARY <romain@eoko.fr>
+ * @copyright  2011 The Eoko Group <http://eoko-studio.fr>
+ * @license    http://www.gnu.org/licenses/gpl.txt  see GPL v3
+ * @version    v1.0
+ * @link       Lien du fichier de description
+ * 
+ */
 
 abstract class Singleton {
 	
