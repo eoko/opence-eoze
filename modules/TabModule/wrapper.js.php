bf10ec4ba85bb5371720ae3619987440615c19ff
?ric Ort?ga <eric@planysphere.fr>
2011-04-22 04:15:44 +0200

    TabModule: creation

---
+++ modules/TabModule/wrapper.js.php
@@ -0,0 +1,50 @@
+(function() {
+
+var <?php echo $var ?> = eo.Class({
+
+	open: function() {
+		if (!this.tab) {
+			Oce.mx.application.getMainDestination().add(this.createTab());
+		}
+		this.tab.show();
+	}
+
+<?php if (isset($config)): ?>
+	,config: <?php echo $config ?>
+
+<?php endif ?>
+
+	,createTab: function() {
+		var tab = this.tab = Ext.create(this.createTabConfig());
+		tab.on('close', function() {
+			delete this.tab;
+		}, this);
+		return tab;
+	}
+
+	,createTabConfig: function() {
+		return {
+			xtype: "panel"
+			,closable: true
+<?php if ($iconCls): ?>
+			,iconCls: "<?php echo $iconCls ?>"
+<?php endif ?>
+		};
+	}
+});
+	
+<?php if (isset($main)): ?>
+
+var spp = <?php echo $var ?>.prototype;
+<?php echo $var ?> = <?php echo $var ?>.extend({});
+
+// === Auto included part ======================================================
+
+<?php echo $main ?>
+
+<?php endif ?>
+
+Ext.ns("<?php echo $namespace ?>.<?php echo $module ?>").<?php echo $module ?> = <?php echo $var ?>;
+Oce.deps.reg('<?php echo "$namespace.$module.$module" ?>');
+
+})(); // closure
