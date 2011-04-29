851fb0b126c3730882706d469a28e2eef710df28
?ric Ort?ga <eric@planysphere.fr>
2011-04-29 18:55:45 +0200

    OCE-25: Ext.form.ComboBox: Allow custom xtype for the PagingToolbar.

--- js/core/overrides.js
+++ js/core/overrides.js
@@ -314,3 +314,116 @@ Ext.grid.Column.types.datecolumn =
 		uber.call(this);
 	};
 })();
+
+/**
+ * @author Éric Ortéga <eric@planysphere.fr>
+ * @since 29/04/11 17:10
+ * 
+ * Allow custom xtype for the PagingToolbar.
+ */
+
+Ext.override(Ext.form.ComboBox, {
+	
+	initList : function(){
+		if(!this.list){
+			var cls = 'x-combo-list',
+			listParent = Ext.getDom(this.getListParent() || Ext.getBody());
+
+			this.list = new Ext.Layer({
+				parentEl: listParent,
+				shadow: this.shadow,
+				cls: [cls, this.listClass].join(' '),
+				constrain:false,
+				zindex: this.getZIndex(listParent)
+			});
+
+			var lw = this.listWidth || Math.max(this.wrap.getWidth(), this.minListWidth);
+			this.list.setSize(lw, 0);
+			this.list.swallowEvent('mousewheel');
+			this.assetHeight = 0;
+			if(this.syncFont !== false){
+				this.list.setStyle('font-size', this.el.getStyle('font-size'));
+			}
+			if(this.title){
+				this.header = this.list.createChild({
+					cls:cls+'-hd', 
+					html: this.title
+					});
+				this.assetHeight += this.header.getHeight();
+			}
+
+			this.innerList = this.list.createChild({
+				cls:cls+'-inner'
+				});
+			this.mon(this.innerList, 'mouseover', this.onViewOver, this);
+			this.mon(this.innerList, 'mousemove', this.onViewMove, this);
+			this.innerList.setWidth(lw - this.list.getFrameWidth('lr'));
+
+			if(this.pageSize){
+				this.footer = this.list.createChild({
+					cls:cls+'-ft'
+					});
+				// <rx+>
+				var xtype = this.pagingToolbarXtype || "paging";
+				if (xtype.xtype) xtype = xtype.xtype;
+				this.pageTb = Ext.create({
+					xtype: xtype,
+					store: this.store,
+					pageSize: this.pageSize,
+					renderTo:this.footer
+				});
+				// </rx+>
+				// <rx->
+				// this.pageTb = new Ext.PagingToolbar({
+				// 	store: this.store,
+				// 	pageSize: this.pageSize,
+				// 	renderTo:this.footer
+				// });
+				// <rx->
+				this.assetHeight += this.footer.getHeight();
+			}
+
+			if(!this.tpl){
+                
+				this.tpl = '{' + this.displayField + '}';
+			}
+            
+			/**
+            * The {@link Ext.DataView DataView} used to display the ComboBox's options.
+            * @type Ext.DataView
+            */
+			this.view = new Ext.DataView({
+				applyTo: this.innerList,
+				tpl: this.tpl,
+				singleSelect: true,
+				selectedClass: this.selectedClass,
+				itemSelector: this.itemSelector || '.' + cls + '-item',
+				emptyText: this.listEmptyText,
+				deferEmptyText: false
+			});
+
+			this.mon(this.view, {
+				containerclick : this.onViewClick,
+				click : this.onViewClick,
+				scope :this
+			});
+
+			this.bindStore(this.store, true);
+
+			if(this.resizable){
+				this.resizer = new Ext.Resizable(this.list,  {
+					pinned:true, 
+					handles:'se'
+				});
+				this.mon(this.resizer, 'resize', function(r, w, h){
+					this.maxHeight = h-this.handleHeight-this.list.getFrameWidth('tb')-this.assetHeight;
+					this.listWidth = w;
+					this.innerList.setWidth(w - this.list.getFrameWidth('lr'));
+					this.restrictHeight();
+				}, this);
+
+				this[this.pageSize?'footer':'innerList'].setStyle('margin-bottom', this.handleHeight+'px');
+			}
+		}
+	}
+});
