f89ffba77e0b9fc2de3dcbf64f4ca1fe48d61bdd
?ric Ort?ga <rixo@protonmail.ch>
2011-06-02 18:52:10 +0200

    index.php: code cleaning

--- php/index.php
+++ php/index.php
@@ -32,30 +32,3 @@ call_user_func(function() {
 	global $directAccess;
 	require_once "{$eoze}php{$ds}init.inc.php";
 });
-//function start() {
-//
-//	$eoze = realpath(dirname(__FILE__) . DS . '..') . DS;
-//	$root = realpath(dirname(__FILE__) . DS . '..' . DS . '..') . DS;
-//
-//	$ds = DS;
-//
-//	require_once "$eoze{$ds}php{$ds}Context.class.php";
-//
-//	$context = new eoko\context\ContextBase();
-//
-//	$context->rootPath = $root;
-//
-//	$context->baseUrl = 'http://' .
-//			(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost')
-//			. rtrim(dirname($_SERVER['PHP_SELF']) , '/\\') . '/' ;
-//
-//	$context->eoze = new eoko\context\Eoze(array(
-//		'namespace' => 'eoko',
-//		'path' => "{$eoze}php$ds",
-//	));
-//
-//	global $directAccess;
-//	require_once "{$eoze}php{$ds}init.inc.php";
-//}
-//
-//start();
