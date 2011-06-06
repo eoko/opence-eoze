3b5719bd5698ca0f9a5122bed1f644704763d19e
?ric Ort?ga <eric@planysphere.fr>
2011-06-06 21:31:18 +0200

    ModuleGroupModule: added support for opening children tabs directory, by module action -- open(dest, *child*)

--- bin/debug.php
+++ bin/debug.php
@@ -4,13 +4,15 @@ require_once 'init.inc.php';
 //Logger::addAppender(new LoggerOutputAppender(false));
 
 Logger::getLogger()->setLevel(Logger::ERROR);
+Logger::getLogger('eoko\cache\Cache')->setLevel(Logger::DEBUG);
 
 use eoko\module\ModuleManager;
 
 $m = ModuleManager::getModule('sm_child_48_products');
 
+//dump($m->getConfig());
 $x = $m->getConfig()->get('extra');
 
-//dump(isset($x['menu']));
+dumpl($x['menu']);
 
-dump($m->getActionProvider()->getFamily());
+//dump($m->getActionProvider()->getFamily());
