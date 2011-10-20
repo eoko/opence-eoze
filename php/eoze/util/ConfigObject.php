38a4136f780cf61019d59bb1a02f3d0860896fb0
?ric Ort?ga <rixo@protonmail.ch>
2011-10-20 09:52:37 +0200

    Added type coercition in create() method

--- php/eoze/util/ConfigObject.php
+++ php/eoze/util/ConfigObject.php
@@ -5,16 +5,25 @@ namespace eoze\util;
 use RuntimeException, IllegalArgumentException;
 
 /**
- *
+ * 
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @author Éric Ortega <eric@planysphere.fr>
  * @since 20 oct. 2011
  */
 abstract class ConfigObject {
 	
-	public static function create() {
+	public static function create(ConfigObject $config = null) {
 		$class = get_called_class();
-		return new $class;
+		if ($config) {
+			if (is_a($config, $class)) {
+				return $config;
+			} else {
+				$actual = get_class($config);
+				throw new IllegalArgumentException("Expects an instance of $class, $actual given");
+			}
+		} else {
+			return new $class;
+		}
 	}
 	
 	public function __call($name, $args) {
