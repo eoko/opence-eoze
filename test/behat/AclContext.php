e12ac2765d3443bc965ecaacb6f3854fb6e1eae2
?ric Ort?ga <rixo@protonmail.ch>
2011-10-20 12:40:07 +0200

    Added AclContext

--- test/behat/AclContext.php
+++ test/behat/AclContext.php
@@ -3,19 +3,79 @@
 namespace eoze\behat\context;
 
 use eoze\test\behat\DatabaseBehatContext;
+use eoze\acl\AclManager;
+use eoze\acl\ArrayManager;
 
 class AclContext extends DatabaseBehatContext {
 
-	protected function getDataSet() {
-		return $this->createYmlDataSet('levels.yml', __DIR__);
+	/**
+	 * @var AclManager
+	 */
+	private $manager = null;
+
+	/**
+	 * @return AclManager
+	 */
+	public function getManager() {
+		if (!$this->manager) {
+			$this->manager = new ArrayManager;
+		}
+		return $this->manager;
+	}
+
+	/**
+	 * @Given /^les roles suivants:$/
+	 */
+	public function lesRolesSuivants(TableNode $table) {
+		foreach ($table->getHash() as $row) {
+			$this->getManager()->newRole($row['id']);
+		}
+	}
+
+	/**
+	 * @Given /^les groupes suivants:$/
+	 */
+	public function lesGroupesSuivants(TableNode $table) {
+		foreach ($table->getHash() as $row) {
+			$manager = $this->getManager();
+			$group = $manager->newGroup($row['id']);
+			if ($row['roles']) {
+				$group->setRoles(explode(',', $row['roles']));
+			}
+		}
+	}
+
+	/**
+	 * @Given /^le role "([^"]*)" existe$/
+	 */
+	public function leRoleExiste($rid) {
+		assertNotNull($this->getManager()->getRole($rid));
+	}
+
+	/**
+	 * @Given /^le groupe "([^"]*)" existe$/
+	 */
+	public function leGroupeExiste($gid) {
+		assertNotNull($this->getManager()->getGroup($gid));
 	}
 
     /**
-     * @Given /^l\'utilisateur est connecté$/
-     */
-    public function lUtilisateurEstConnecte()
-    {
-        throw new PendingException();
-    }
-	
+	 * @Given /^le groupe "([^"]*)" a le role "([^"]*)"$/
+	 */
+	public function leGroupeALeRole($gid, $rid) {
+		assertNotNull($group = $this->getManager()->getGroup($gid));
+		assertTrue($group->hasRole($rid));
+	}
+
+//	protected function getDataSet() {
+//		return $this->createYmlDataSet('levels.yml', __DIR__);
+//	}
+//
+//    /**
+//     * @Given /^l\'utilisateur est connecté$/
+//     */
+//    public function lUtilisateurEstConnecte()
+//    {
+//        throw new PendingException();
+//    }
 }
