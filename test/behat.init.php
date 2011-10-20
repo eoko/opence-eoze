dd371aa1ffcda397e60456198fb455019ae61927
?ric Ort?ga <rixo@protonmail.ch>
2011-10-20 03:24:21 +0200

    Fixed testing boiler plates against real application (OpenCE)

--- test/behat.init.php
+++ test/behat.init.php
@@ -2,18 +2,16 @@
 
 call_user_func(function() {
 	
-	$ds = DIRECTORY_SEPARATOR;
-	
 	$featureNamespace = 'eoze\\behat\\context\\';
 	$strlenFNS = strlen($featureNamespace);
 	
-	$contextPath = __DIR__ . $ds . 'behat' . $ds;
+	$contextPath = __DIR__ . '/behat/';
 	define('EOZE_FEATURES_PATH', $contextPath);
-
+	
 	// Register autoload for eoze Context classes
-	spl_autoload_register(function($class) use($ds, $contextPath, $featureNamespace, $strlenFNS) {
+	spl_autoload_register(function($class) use($contextPath, $featureNamespace, $strlenFNS) {
 		if (substr($class, 0, $strlenFNS) === $featureNamespace) {
-			$classPath = str_replace('\\', $ds, substr($class, $strlenFNS));
+			$classPath = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, $strlenFNS));
 			if (file_exists($file = $contextPath . $classPath . '.php')
 					|| file_exists($file = $contextPath . $classPath . 'Context.php')) {
 				require $file;
@@ -21,7 +19,7 @@ call_user_func(function() {
 		}
 	});
 	
-	require __DIR__ . $ds . 'php' . $ds . 'init.inc.php';
+	require __DIR__ . '/php/init.inc.php';
 
 	// PHPUnit
 	require_once 'PHPUnit/Autoload.php';
