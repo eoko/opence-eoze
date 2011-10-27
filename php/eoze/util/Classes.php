cde42e5e111f5ef27be9281bed8e67d17fe790f5
?ric Ort?ga <eric@planysphere.fr>
2011-10-27 20:50:06 +0200

    Added doc & getParentNames() method

--- php/eoze/util/Classes.php
+++ php/eoze/util/Classes.php
@@ -5,7 +5,13 @@ namespace eoze\util;
 use ReflectionClass;
 
 /**
- *
+ * Utility class providing helper methods to get informations about classes,
+ * mostly by using reflection.
+ * 
+ * All methods of this class that take an argument $class accept either a
+ * string representing the class name, or a {@link ReflectionClass} object,
+ * or an object which class will be extracted with {@link get_class()}.
+ * 
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @author Éric Ortega <eric@planysphere.fr>
  * @since 27 oct. 2011
@@ -14,15 +20,44 @@ class Classes {
 	
 	private function __construct() {}
 	
-	public static function getImplementedInterfaces($class) {
-		$rc = new ReflectionClass($class);
-		$r = array($class => true);
-		foreach ($rc->getInterfaceNames() as $interface) {
-			$r[$interface] = true;
+	private static function toReflectionClass(&$class) {
+		if ($class instanceof ReflectionClass) {
+			$rc = $class;
+			$class = $class->name;
+			return $rc;
+		} else {
+			if (is_object($class)) {
+				$class = get_class($class);
+			}
+			return new ReflectionClass($class);
 		}
+	}
+
+	public static function getParentNames($class) {
+		if (!($class instanceof ReflectionClass)) {
+			$rc = new ReflectionClass($class);
+		}
+		$r = array();
 		while ($rc = $rc->getParentClass()) {
-			$r[$rc->getName()] = true;
+			$r[] = $rc->getName();
 		}
+		return $r;
+	}
+	
+	/**
+	 * Gets the names of all the interfaces that are implemented by the given $class,
+	 * and also the names of all its parent class.
+	 * 
+	 * @param string|Object|ReflectionClass $class
+	 * @param bool $includeSelf TRUE to include the name of the explored $class in the
+	 * returned array.
+	 * @return array An array containing the names of all implemented interfaces as strings.
+	 */
+	public static function getImplementedInterfaces($class, $includeSelf = false) {
+		$rc = self::toReflectionClass($class);
+		$r = $includeSelf ? array($class) : array();
+		$r = array_merge($r, $rc->getInterfaceNames());
+		$r = array_merge($r, self::getParentNames($class));
 		return array_keys($r);
 	}
 }
