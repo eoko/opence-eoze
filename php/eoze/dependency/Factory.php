b84f14b691eb2a8be5b966ada598eefc8e162bc7
?ric Ort?ga <rixo@protonmail.ch>
2011-10-27 21:50:18 +0200

    Added eoze\dependency\Registry & dependencies & tests

---
+++ php/eoze/dependency/Factory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace eoze\dependency;
+
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 27 oct. 2011
+ */
+interface Factory {
+	
+	function create(array $config = null);
+}
