72b3ad1d99c07e3059ef09a2769d08c931121819
?ric Ort?ga <eric@planysphere.fr>
2011-11-01 22:23:33 +0100

    Added DateTimeProvider

---
+++ php/eoze/DateTime/TimeZoneProvider/AbstractTimeZoneProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace eoze\DateTime\TimeZoneProvider;
+
+use eoze\DateTime\TimeZoneProvider;
+
+use DateTimeZone;
+
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 1 nov. 2011
+ */
+abstract class AbstractTimeZoneProvider implements TimeZoneProvider {
+
+	private static $utcTimeZone;
+	
+	public function getUtcName() {
+		return 'UTC';
+	}
+	
+	public function getUtc() {
+		if (!self::$utcTimeZone) {
+			self::$utcTimeZone = new DateTimeZone($this->getUtcName());
+		}
+		return self::$utcTimeZone;
+	}
+	
+}
