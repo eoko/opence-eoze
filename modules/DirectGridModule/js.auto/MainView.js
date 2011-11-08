ddc87a103a4497332ddaaa43aa1831c524452c02
?ric Ort?ga <eric@planysphere.fr>
2011-11-08 05:36:04 +0100

    Added DirectModule

---
+++ modules/DirectGridModule/js.auto/MainView.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 8 nov. 2011
+ */
+Oce.deps.wait("eo.DirectModule.MainView", function() {
+	
+Ext.ns('eo.DirectGridModule');
+
+eo.DirectGridModule.MainView = Ext.extend(eo.DirectModule.MainView, {
+	
+	
+});
+}); // deps
