d9a3381346aa663322fcfaa896bcc5da798f3ae0
?ric Ort?ga <rixo@protonmail.ch>
2011-11-16 06:42:39 +0100

    Created advanced contact fields

---
+++ js/components/eo/form/contact/AddressFieldSet.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 15 nov. 2011
+ */
+Oce.deps.waitIn('eo.form.contact', 'AbstractFieldSet', function(NS) {
+	
+var sp  = NS.AbstractFieldSet,
+	spp = sp.prototype;
+
+NS.AddressFieldSet = Ext.extend(sp, {
+
+	fieldConfig: NS.config.address
+
+	,getFieldClass: function() {
+		return NS.AddressField;
+	}
+});
+
+Ext.reg('addressesfieldset', NS.AddressFieldSet);
+	
+}); // deps
---
+++ js/components/eo/form/contact/PhoneNumberField.js
@@ -0,0 +1,56 @@
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 15 nov. 2011
+ */
+Oce.deps.waitIn('eo.form.contact', 'AbstractContactField', function(NS, ns) {
+
+var sp  = NS.AbstractContactField,
+	spp = sp.prototype;
+
+NS.PhoneNumberField = Ext.extend(sp, Ext.apply({
+	
+	textUnlisted: NS.locale('unlisted')
+	,textItem: NS.locale('number')
+	
+	,numberName:   'number'
+	,unlistedName: 'unlisted'
+	
+	,initComponent: function() {
+		
+		this.numberFormField = new Ext.form.TextField({
+			emptyText: this.textItem
+			,allowBlank: false
+		});
+		
+		this.unlistedCheckbox = new Ext.form.Checkbox({
+			boxLabel: this.textUnlisted
+		});
+		
+		this.numberFormField.flex = 1;
+	
+		var items = [
+			this.numberFormField,
+			this.unlistedCheckbox
+		];
+		
+		Ext.apply(this, {
+			items: items
+		});
+		
+		spp.initComponent.call(this);
+		
+		this.valueFields[this.numberName] = this.numberFormField;
+		this.valueFields[this.unlistedName] = this.unlistedCheckbox;
+	}
+	
+	,isValid: function() {
+		return this.numberFormField.getValue();
+	}
+	
+}, NS.config.phone));
+
+Ext.reg('phonenumberfield', NS.PhoneNumberField);
+
+}); // deps
