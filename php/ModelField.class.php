d12eaaf33fe25ea7e13a3bd4e632724deaf60b0d
?ric Ort?ga <eric@planysphere.fr>
2011-11-22 00:20:16 +0100

    Added castValue to ModelField

--- php/ModelField.class.php
+++ php/ModelField.class.php
@@ -2,6 +2,19 @@
 
 interface ModelField {
 
+	const T_INT = 'int';
+	const T_INTEGER = 'int';
+	const T_STRING = 'string';
+	const T_TEXT = 'text';
+	const T_DATE = 'date';
+	const T_TIME = 'time';
+	const T_DATETIME = 'datetime';
+	const T_BOOL = 'bool';
+	const T_BOOLEAN = 'bool';
+	const T_FLOAT = 'float';
+	const T_DECIMAL = 'decimal';
+	const T_ENUM = 'enum';
+
 	function getName();
 
 //	function select(ModelTableQuery $query);
@@ -11,4 +24,6 @@ interface ModelField {
 	function getType();
 
 	function isNullable();
+	
+	function castValue($value);
 }
---
+++ php/ModelFieldBase.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Base class implementation for {@link ModelField}.
+ * 
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 22 nov. 2011
+ */
+abstract class ModelFieldBase implements ModelField {
+	
+	public function castValue($value) {
+		switch ($this->getType()) {
+			case ModelField::T_INT:
+				return (int) $value;
+			case ModelField::T_BOOL:
+				return (bool) $value;
+			case ModelField::T_FLOAT:
+				return (double) $value;
+			case ModelField::T_DATE:
+			case ModelField::T_DATETIME:
+//				throw new UnsupportedOperationException('Not implemented yet');
+			case ModelField::T_DECIMAL:
+			case ModelField::T_STRING:
+			case ModelField::T_TEXT:
+				return $value;
+			default:
+				throw new Exception('Unknown type: ' . $this->getType());
+				return $value;
+		}
+	}
+}
