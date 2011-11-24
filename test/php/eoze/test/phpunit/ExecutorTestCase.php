6830c30a318a1b83cd0e017ea2feba0126026262
?ric Ort?ga <rixo@protonmail.ch>
2011-11-24 01:34:12 +0100

    Fixed getController

--- test/php/eoze/test/phpunit/ExecutorTestCase.php
+++ test/php/eoze/test/phpunit/ExecutorTestCase.php
@@ -30,29 +30,29 @@ class ExecutorTestCase extends ModuleTestCase {
 	protected $module;
 	protected $moduleName;
 	
+	protected $controller;
+	
+	public function __construct() {
+		if (isset($this->controller)) {
+			switch (count($parts = explode('.', $this->controller))) {
+				case 2:
+					if (!$this->executorType) {
+						$this->executorType = $parts[1];
+					}
+				case 1:
+					if (!$this->moduleName) {
+						$this->moduleName = $parts[0];
+					}
+					break;
+				default:
+					throw new IllegalStateException('Invalid controller: ' . $this->controller);
+			}
+		}
+	}
+	
 	protected function setUp() {
 		parent::setUp();
-		$this->module = $this->createModule();
-	}
-	
-	/**
-	 *
-	 * @return Module
-	 */
-	protected function getModule() {
-		return ModuleManager::getModule($this->getModuleName());
-	}
-	
-	/**
-	 * @param string  $action
-	 * @param Request $request
-	 * @param bool    $internal
-	 * 
-	 * @return Executor
-	 */
-	protected function createExecutor($action = null, Request $request = null, $internal = false) {
-		return $this->getModule()->createExecutor(
-				$this->getExecutorType(), $action, $request, $internal);
+		$this->module = $this->getModule();
 	}
 	
 	protected function getModuleName() {
@@ -78,6 +78,10 @@ class ExecutorTestCase extends ModuleTestCase {
 		if (isset($this->executorType)) {
 			return $this->executorType;
 		}
+		if (isset($this->controller)) {
+			$parts = explode('.', $this->controller);
+			return $this->executorType = $parts[1];
+		}
 		$p = new ReflectionProperty($this, 'executor');
 		$comment = $p->getDocComment();
 		if ($comment 
@@ -94,5 +98,46 @@ class ExecutorTestCase extends ModuleTestCase {
 			}
 		}
 		throw new IllegalStateException('Cannot determine executor type');
+	}
+	
+	protected function getControllerName() {
+		if (!$this->controller) {
+			return $this->controller = $this->getModuleName() . '.' . $this->getExecutorType();
+		} else {
+			return $this->controller;
+		}
+	}
+	
+	/**
+	 *
+	 * @return Module
+	 */
+	protected function getModule() {
+		return ModuleManager::getModule($this->getModuleName());
+	}
+	
+	/**
+	 * @param string  $action
+	 * @param Request $request
+	 * @param bool    $internal
+	 * 
+	 * @return Executor
+	 */
+	protected function createExecutor($action = null, Request $request = null, $internal = false) {
+		return $this->getModule()->createExecutor(
+				$this->getExecutorType(), $action, $request, $internal);
+	}
+	
+	/**
+	 * Runs the given request on the Executor tested by this test case.
+	 * 
+	 * If the controller param of the request array is not set, it will be set
+	 * automatically.
+	 * 
+	 * @param array $request
+	 * @param bool $autoController 
+	 */
+	protected function runRequest(array $request) {
+		
 	}
 }
