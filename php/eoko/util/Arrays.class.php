ac5a9a56f07b3535972700906938bb24e9c5fc70
?ric Ort?ga <rixo@protonmail.ch>
2011-11-24 18:04:18 +0100

    Added compareMap method

--- php/eoko/util/Arrays.class.php
+++ php/eoko/util/Arrays.class.php
@@ -4,4 +4,32 @@ namespace eoko\util;
 
 class Arrays extends \ArrayHelper {
 	
+	public static function compareMap(array $left = null, array $right = null) {
+		if ($left === null || $right === null) {
+			return $left === $right;
+		}
+		if (self::isAssoc($left) && !self::isAssoc($right)) {
+			return false;
+		}
+		return self::orderMapAs($left, $right) === $right;
+	}
+	
+	public static function orderMapAs(array $map, array $format) {
+		$return = array();
+		foreach ($format as $key => $v) {
+			if (array_key_exists($key, $map)) {
+				if (self::isAssocArray($map[$key]) && self::isAssocArray($format[$key])) {
+					$return[$key] = self::orderMapAs($map[$key], $format[$key]);
+				} else {
+					$return[$key] = $map[$key];
+				}
+				unset($map[$key]);
+			}
+		}
+		foreach ($map as $key => $value) {
+			$return[$key] = $value;
+		}
+		return $return;
+	}
+	
 }
