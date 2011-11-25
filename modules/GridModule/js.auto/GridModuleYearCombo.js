9dce54d7f20fc202d7463d0dcb75d295c14285d2
?ric Ort?ga <rixo@protonmail.ch>
2011-11-25 02:56:04 +0100

    Progressing on OCU-61

--- modules/GridModule/js.auto/GridModuleYearCombo.js
+++ modules/GridModule/js.auto/GridModuleYearCombo.js
@@ -2,98 +2,105 @@ Ext.ns('Oce.GridModule')
 
 Oce.deps.wait('Oce.form.ForeignComboBox', function() {
 
-	Oce.YearCombo = Ext.extend(Oce.form.ForeignComboBox, {
+//	Oce.YearCombo = Ext.extend(Oce.form.ForeignComboBox, {
+//
+//		label: 'Année'
+//
+//		,constructor: function(config) {
+//			
+//			this.addEvents({
+//				'yearchanged': true
+//			});
+//
+//			Ext.applyIf(config, {
+//				fieldLabel: this.label
+//				,controller: 'years'
+//				,clearable: false
+//	//			,listeners: {
+//	//				select: function(combo, record, index) {
+//	//					this.onValueChange(record.data.id);
+//	//				}.createDelegate(this)
+//	//			}
+//
+//				,forceSelection: true
+//			});
+//
+//			this.on('select', function(combo, record, index) {
+//				this.onValueChange(record.data.id);
+//			}.createDelegate(this))
+//
+//			this.addEvents('firstload');
+//
+//			this.firstLoadListener = function() {
+//				this.store.un('load', this.firstLoadListener);
+//				this.firstLoadListener = true;
+//				this.fireEvent('firstload');
+//			}.createDelegate(this);
+//
+//			Oce.YearCombo.superclass.constructor.call(this, config);
+//
+//			this.whenStoreLoaded(this.firstLoadListener);
+//
+//			this.setValue(config.year);
+//		}
+//
+//		,waitFirstLoad: function(callback) {
+//			if (this.firstLoadListener === true) {
+//				callback();
+//			} else {
+//				this.on('firstload', callback);
+//			}
+//		}
+//
+//		,onValueChange: function(year) {
+//			this.fireEvent('yearchanged', year);
+//		}
+//
+//		,setValue: function(v) {
+//			if (this.store != null) {
+//				Oce.YearCombo.superclass.setValue.call(this, v);
+//			} else {
+//				this.value = v;
+//			}
+//		}
+//	})
 
-		label: 'Année'
-
-		,constructor: function(config) {
-
-			this.addEvents({
-				'yearchanged': true
-			});
-
-			Ext.applyIf(config, {
-				fieldLabel: this.label
-				,controller: 'years'
-				,clearable: false
-	//			,listeners: {
-	//				select: function(combo, record, index) {
-	//					this.onValueChange(record.data.id);
-	//				}.createDelegate(this)
-	//			}
-
-				,forceSelection: true
-			});
-
-			this.on('select', function(combo, record, index) {
-				this.onValueChange(record.data.id);
-			}.createDelegate(this))
-
-			this.addEvents('firstload');
-
-			this.firstLoadListener = function() {
-				this.store.un('load', this.firstLoadListener);
-				this.firstLoadListener = true;
-				this.fireEvent('firstload');
-			}.createDelegate(this);
-
-			Oce.YearCombo.superclass.constructor.call(this, config);
-
-			this.whenStoreLoaded(this.firstLoadListener);
-
-			this.setValue(config.year);
-		}
-
+	Oce.YearCombo = Ext.extend(Ext.form.DateField, {
+		onValueChange: function(year) {}
 		,waitFirstLoad: function(callback) {
-			if (this.firstLoadListener === true) {
-				callback();
-			} else {
-				this.on('firstload', callback);
-			}
+			callback();
 		}
-
-		,onValueChange: function(year) {
-			this.fireEvent('yearchanged', year);
-		}
-
-		,setValue: function(v) {
-			if (this.store != null) {
-				Oce.YearCombo.superclass.setValue.call(this, v);
-			} else {
-				this.value = v;
-			}
-		}
-	})
+	});
 
 	Ext.reg('oce.yearcombo', Oce.YearCombo);
 
 	Oce.GridModuleYearCombo = Ext.extend(Oce.YearCombo, {
 
-		constructor: function(config) {
+//		constructor: function(config) {
+//
+//			Oce.GridModuleYearCombo.superclass.constructor.call(this, config);
+//
+//			if (false === 'module' in config) throw new 'Missing required config param: module';
+//
+//			this.module = config.module;
+//
+//			var superOnRead = this.module.store.proxy.onRead;
+//
+//			this.module.store.proxy.onRead = function(action, trans, result, res) {
+//
+//				superOnRead.apply(this.module.store.proxy, arguments);
+//
+//				var o = Ext.util.JSON.decode(result.responseText);
+//				this.setValue(o.year);
+//			}.createDelegate(this)
+//
+//			this.on('yearchanged', function(year) {
+//				this.module.store.setBaseParam('year', year);
+//				this.module.store.reload();
+//			}.createDelegate(this))
+//		}
 
-			Oce.GridModuleYearCombo.superclass.constructor.call(this, config);
-
-			if (false === 'module' in config) throw new 'Missing required config param: module';
-
-			this.module = config.module;
-
-			var superOnRead = this.module.store.proxy.onRead;
-
-			this.module.store.proxy.onRead = function(action, trans, result, res) {
-
-				superOnRead.apply(this.module.store.proxy, arguments);
-
-				var o = Ext.util.JSON.decode(result.responseText);
-				this.setValue(o.year);
-			}.createDelegate(this)
-
-			this.on('yearchanged', function(year) {
-				this.module.store.setBaseParam('year', year);
-				this.module.store.reload();
-			}.createDelegate(this))
-		}
-
-	})
+	});
 
 	Ext.reg('oce.gm.yearcombo', Oce.GridModuleYearCombo);
 
