6efe0ffcf2b978301875f6f9c07b4b7e33094cda
?ric Ort?ga <eric@planysphere.fr>
2011-11-30 03:44:53 +0100

    Added message for vfsStream dependency

--- test/phpunit.init.php
+++ test/phpunit.init.php
@@ -7,3 +7,20 @@ if (class_exists('DatabaseTestCase')) {
 } else {
 	class_alias('eoze\\test\\phpunit\\DatabaseTestCase', 'DatabaseTestCase');
 }
+
+// Ensure vfsStream is installed
+include_once 'vfsStream/vfsStream.php';
+
+if (!class_exists('vfsStreamWrapper')) {
+	exit(<<<ERROR
+vfsStream required for eoze tests:
+
+You can install it with pear:
+
+$ pear channel-discover pear.php-tools.net
+$ pear install pat/vfsStream-beta
+
+See: https://github.com/mikey179/vfsStream/wiki/Install
+ERROR
+	);
+}
