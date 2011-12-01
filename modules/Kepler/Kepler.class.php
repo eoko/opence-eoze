77ba667ae02d0180d4ca290775e594a947266074
?ric Ort?ga <eric@planysphere.fr>
2011-12-01 06:56:56 +0100

    Added module Kepler

---
+++ modules/Kepler/Kepler.class.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace eoko\modules\Kepler;
+
+use eoko\module\Module;
+
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 30 nov. 2011
+ */
+class Kepler extends Module {
+	
+	protected $defaultExecutor = 'json';
+	
+	private $workingPath;
+	
+	public function getWorkingPath($subDirectory = null) {
+		if ($this->workingPath === null) {
+			if (null !== $path = $this->getConfig()->getValue('workingPath')) {
+				$this->workingPath = $this->replacePathVariables($path);
+			} else {
+				throw new \MissingConfigurationException();
+			}
+		}
+		if ($subDirectory) {
+			return $this->workingPath . '/' . ltrim($subDirectory, '\/');
+		} else {
+			return $this->workingPath;
+		}
+	}
+	
+	private function replacePathVariables($path) {
+		$search  = array();
+		$replace = array();
+		foreach (array(
+			'%var%' => MY_EOZE_PATH,
+		) as $var => $rep) {
+			$search[]  = $var;
+			$replace[] = rtrim($rep, '\/');
+		}
+		return str_replace($search, $replace, $path);
+	}
+	
+	public function buildCometEntries(array $entries) {
+		$result = array();
+		foreach ($entries as $entry) {
+			$result[$entry->category][] = $entry->data;
+		}
+		return $result;
+	}
+}
