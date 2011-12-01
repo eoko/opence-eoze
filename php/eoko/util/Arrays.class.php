90788fe2ff23660dc5aa6a0e04ae63c8f9d063e4
?ric Ort?ga <eric@planysphere.fr>
2011-12-01 06:04:13 +0100

    Doc

--- php/eoko/util/Arrays.class.php
+++ php/eoko/util/Arrays.class.php
@@ -4,6 +4,21 @@ namespace eoko\util;
 
 class Arrays extends \ArrayHelper {
 	
+	/**
+	 * Compares two array considered as hash map, that is the order
+	 * of the elements is not taken into account.
+	 * 
+	 * This method will return `true` if the two array contains the 
+	 * same number of elements, the same keys, and the key are 
+	 * associated to the same values (or, also, if the two variables 
+	 * to compare are `null`).
+	 * 
+	 * @param array $left  
+	 * @param array $right 
+	 * @param bool $strict Use the strict comparison operator (`===`) 
+	 * to evaluate equality between elements.
+	 * @return type 
+	 */
 	public static function compareMap(array $left = null, array $right = null, $strict = true) {
 		if ($left === null || $right === null) {
 			return $left === $right;
