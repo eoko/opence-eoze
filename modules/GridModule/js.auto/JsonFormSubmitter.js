fbac348d6058c1e92c664f4244863e4364b400f5
?ric Ort?ga <rixo@protonmail.ch>
2011-12-04 04:42:19 +0100

    Cleaning repo

--- modules/GridModule/js.auto/JsonFormSubmitter.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * @author Éric Ortega <eric@planysphere.fr>
- * @since 2 déc. 2011
- */
-Ext.ns('Oce.Modules.GridModule');
-
-//Oce.Modules.GridModule.JsonFormSubmitter = 
