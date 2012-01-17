d13cd08b12b0cd9b7ecc4a862b09082cd39cba4a
?ric Ort?ga <rixo@protonmail.ch>
2012-01-17 06:03:01 +0100

    Version creation algorithm.

--- bin/debug.php
+++ bin/debug.php
@@ -6,32 +6,22 @@ require_once 'init.inc.php';
 Logger::getLogger()->setLevel(Logger::ERROR);
 Logger::getLogger('eoko\cache\Cache')->setLevel(Logger::DEBUG);
 
-//require_once 'Benchmark/Iterate.php';
-//
-//$benchmark = new Benchmark_Iterate;
-//
-//$parent = array(
-//	'key1' => 1,
-//	'key2' => 2,
-//	'indexed' => array(1,2,3),
-//	'sub' => array(
-//		'key1' => 1,
-//		'key2' => 2,
-//		'indexed' => array(1,2,3),
-//	)
-//);
-//
-//$child = array(
-//	'key2' => 3,
-//	'key3' => 3,
-//	'indexed[]' => array(4,5,6),
-//	'sub' => array(
-//		'key2' => 3,
-//		'key3' => 3,
-//		'indexed' => array(4,5,6),
-//	)
-//);
-//
-//$benchmark->run(10000, 'eoze\Config\Helper::extend', $parent, $child);
-//
-//print_r($benchmark->get());
+foreach (ContactTable::findAll() as $contact) {
+	$contact instanceof Contact;
+	
+	$version = DataVersion::create();
+	
+	$rev = DataVersionRevision::create(array(
+		'version_id' => $version->getId(),
+		'datetime'   => date('Y-m-d H:i:s'), // now
+		'user_id'    => 97,
+		'action'     => 'CREATE',
+	));
+	
+	$version->setLastRevisionNumber(1);
+	$version->setRevisions(array($rev));
+//	$version->addRevision('CREATE');
+	
+	$contact->setVersion($version);
+	$contact->save();
+}
