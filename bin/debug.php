d9fe3e6b70458c7e4f135b56a6c97d9505a1d216
?ric Ort?ga <rixo@protonmail.ch>
2012-01-17 07:59:35 +0100

    Updating models.

--- bin/debug.php
+++ bin/debug.php
@@ -5,23 +5,43 @@ require_once 'init.inc.php';
 
 Logger::getLogger()->setLevel(Logger::ERROR);
 Logger::getLogger('eoko\cache\Cache')->setLevel(Logger::DEBUG);
+\eoko\modules\Kepler\CometEvents::disable();
 
-foreach (ContactTable::findAll() as $contact) {
-	$contact instanceof Contact;
+createVersions(MemberTable::getInstance());
+
+function setPrimaryPhoneNumbers() {
+
+	$contactIds = ContactPhoneNumberTable::createQuery()
+			->select('contact_id')
+			->executeSelectColumn();
+
+	foreach ($contactIds as $cid) {
+		$pn = ContactPhoneNumberTable::findFirstWhere("`contact_id` = ?", $cid);
+		$pn->setPrimary(true);
+		$pn->save();
+	}
+}
+
+function createVersions(ModelTable $table) {
 	
-	$version = DataVersion::create();
-	
-	$rev = DataVersionRevision::create(array(
-		'version_id' => $version->getId(),
-		'datetime'   => date('Y-m-d H:i:s'), // now
-		'user_id'    => 97,
-		'action'     => 'CREATE',
-	));
-	
-	$version->setLastRevisionNumber(1);
-	$version->setRevisions(array($rev));
-//	$version->addRevision('CREATE');
-	
-	$contact->setVersion($version);
-	$contact->save();
+	foreach ($table::findAll() as $record) {
+		
+		$version = DataVersion::create();
+
+		$rev = DataVersionRevision::create(array(
+			'version_id' => $version->getId(),
+			'datetime'   => date('Y-m-d H:i:s'), // now
+			'user_id'    => 97,
+			'action'     => 'CREATE',
+		));
+
+		$version->setLastRevisionNumber(1);
+		$version->setRevisions(array($rev));
+	//	$version->addRevision('CREATE');
+
+		$record->setVersion($version);
+		$record->save();
+		
+		echo "Record $record->id done.\n";
+	}
 }
