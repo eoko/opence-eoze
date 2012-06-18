4284cfeca036901698102aeee9e03e74602c5f12
?ric Ort?ga <eric@planysphere.fr>
2012-06-18 13:34:57 +0200

    Fixed selectedId & removed debugger.

--- js/components/eo/form/ComboBox.js
+++ js/components/eo/form/ComboBox.js
@@ -165,16 +165,15 @@ eo.form.ComboBox = Ext.extend(Ext.form.ComboBox, {
 	}
 	
 	// overridden to update selectedId on setValue
-	,setValue: function() {
+	,setValue: function(v) {
 		var r = eo.form.ComboBox.superclass.setValue.apply(this, arguments);
-		this.selectedId = this.getValue();
+		this.selectedId = v;
 		return r;
 	}
 	
 	// for getSelectedId()
 	// private
     ,onSelect: function(record, index){
-		debugger
 		this.selectedId = record && record.id;
 		eo.form.ComboBox.superclass.onSelect.apply(this, arguments);
     }
