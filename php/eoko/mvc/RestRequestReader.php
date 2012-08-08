1324afdcf06cc4bbc1920ce64e22179cc56974a8
?ric Ort?ga <rixo@protonmail.ch>
2012-08-08 12:52:42 +0200

    Implemented RestRequestReader.

--- php/eoko/mvc/RestRequestReader.php
+++ php/eoko/mvc/RestRequestReader.php
@@ -2,6 +2,8 @@
 
 namespace eoko\mvc;
 
+use Request as RequestData;
+
 /**
  *
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
@@ -11,6 +13,28 @@ namespace eoko\mvc;
 class RestRequestReader extends AbstractRequestReader {
 
 	public function createRequest() {
-		throw new \eoze\Exception\NotImplementedYetException;
+		$data = array();
+		
+		foreach ($this->routeMatch->getParams() as $key => $value) {
+			if (substr($key, 0, 1) !== '_') {
+				$data[$key] = $value;
+			}
+		}
+		
+		$contentType = $this->request->getHeader('content-type');
+		if ($contentType) {
+			if ($contentType->getFieldValue() === 'application/json') {
+				foreach (json_decode($this->request->getContent(), true) as $k => $v) {
+					$data[$k] = $v;
+				}
+			}
+		} else {
+			throw new \UnsupportedOperationException(
+				'Unsupported request content-type (ie. unsupported message format): '
+				. $contentType->getFieldValue()
+			);
+		}
+		
+		return new RequestData($data);
 	}
 }
