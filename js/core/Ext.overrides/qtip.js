22b83c4b418a32ff409137fd39a054f2d71b9e96
?ric Ort?ga <eric@planysphere.fr>
2012-08-16 16:17:45 +0200

    Added comment.

--- js/core/Ext.overrides/qtip.js
+++ js/core/Ext.overrides/qtip.js
@@ -9,6 +9,9 @@
 	
 var clear = Ext.form.MessageTargets.qtip.clear;
 
+// Ensures that the target element still exists before clearing.
+// The target element may indeed have already been destroyed, if the clear method
+// is called in a delayed task (which is the case in the code of Ext itself).
 Ext.form.MessageTargets.qtip.clear = function(field) {
 	if (field.el) {
 		clear.apply(this, arguments);
