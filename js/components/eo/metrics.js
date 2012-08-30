d94e20f9e77997064d58cfea5d9453b7ab89b85e
?ric Ort?ga <rixo@protonmail.ch>
2012-08-30 12:49:31 +0200

    Added metrics helper.

---
+++ js/components/eo/metrics.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 30 août 2012
+ */
+Ext.ns('eo');
+
+eo.metrics = {
+	
+	calcActionColumnWidth: function(itemsNumber, iconWidth, margin) {
+		var n = Ext.isDefined(itemsNumber) ? itemsNumber : 1,
+			w = Ext.isDefined(iconWidth) ? iconWidth : 16,
+			m = Ext.isDefined(margin) ? margin : 5;
+		return n * (w + m) + m;
+	}
+	
+};
