c9661078326d3cb30af746ced05a9c1afb68c325
?ric Ort?ga <eric@planysphere.fr>
2012-09-06 23:25:57 +0200

    Done OCE-116.

---
+++ modules/UserPreferences/UserPreferences.class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace eoko\modules\UserPreferences;
+
+use eoko\module\Module;
+
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 5 sept. 2012
+ */
+class UserPreferences extends Module {
+
+	protected $defaultExecutor = 'json';
+}
