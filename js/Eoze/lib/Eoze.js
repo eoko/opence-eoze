1bbca9929b7c0dff82fcf0f469821c9839ff6e17
?ric Ort?ga <eric@planysphere.fr>
2012-10-05 03:13:38 +0200

    OCE-575 Creating Eoze support classes for Ext4.

---
+++ js/Eoze/lib/Eoze.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 5 oct. 2012
+ */
+Ext4.define('Eoze.lib.Eoze', {
+	requires: [
+		// adds support for lazy instance creation by xclass
+		'Eoze.Ext.ComponentManager',
+		// i18n
+		'Eoze.i18n.Locale',
+		// Locale
+		'Eoze.locale.fr.Locale'
+	]
+});
---
+++ js/init/Ext4Application.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @author Éric Ortega <eric@planysphere.fr>
+ * @since 3 oct. 2012
+ */
+if (window.Ext4) {
+	Ext4.application({
+		
+		name: 'Eoze'
+		
+		,requires: [
+			'Eoze.lib.Eoze',
+		]
+	});	
+}
