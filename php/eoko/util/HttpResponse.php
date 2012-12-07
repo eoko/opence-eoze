1cb2e9ca9b9a04c88716a7b3364235f5bcfaf716
?ric Ort?ga <eric@planysphere.fr>
2012-12-07 13:47:14 +0100

    Fixed header sending.

--- php/eoko/util/HttpResponse.php
+++ php/eoko/util/HttpResponse.php
@@ -7,7 +7,7 @@ class HttpResponse {
 	private function __construct() {}
 
 	public static function answer404($die = true) {
-		header('404 Not Found');
+		header('Status: 404 Not Found');
 		echo '<h1>404 Not Found</h1>';
 		if ($die) die;
 	}
