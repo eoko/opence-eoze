f6a0eae533e73c61ca0ecc4dfbbadaf0c6e4b15b
?ric Ort?ga <rixo@protonmail.ch>
2012-12-18 12:51:12 +0100

    Extracted Ext.data.Types overrides.

--- js/core/overrides.js
+++ js/core/overrides.js
@@ -738,31 +738,6 @@ Ext.form.TextField.prototype.selectOnFocus = true;
 	});
 })(Ext.form.TextField.prototype.filterValidation);
 
-// Fix Int data type to actually return NULL if the value cannot be parsed
-Ext.data.Types.INT.convert = function(v) {
-	var r = v !== undefined && v !== null && v !== '' ?
-			parseInt(String(v).replace(Ext.data.Types.stripRe, ''), 10) 
-			: (this.useNull ? null : 0); 
-	return Ext.isNumber(r) ? r : (this.useNull ? null : 0);
-};
-
-(function() {
-	var o = Ext.data.Types.DATE,
-		uber = o.convert,
-		iso = {
-			dateFormat: 'Y-m-d\\TH:i:s'
-		},
-		sql = {
-			dateFormat: 'Y-m-d H:i:s'
-		};
-		
-	o.convert = function(v) {
-		if (!v) return null;
-		if (Ext.isDate(v)) return v;
-		return uber.call(this, v) || uber.call(iso, v) || uber.call(sql, v);
-	}
-})();
-
 /**
  * @todo Check other browser (safari, probably...)
  * With chrome, for a reason I don't get, the wrapping .x-form-element div
