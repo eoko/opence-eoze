331bae2d86aae847f83a6c9d26fec1ae6114d533
?ric Ort?ga <rixo@protonmail.ch>
2013-02-16 08:04:10 +0100

    Left deprecation message.

--- test/php/init.inc.php
+++ test/php/init.inc.php
@@ -18,6 +18,7 @@ require_once __DIR__
 restore_error_handler();
 restore_exception_handler();
 
+// TODO CACHE_PATH was deprecated on 2013-02-16, use Application::resolvePath() instead
 define('TMP_DIR', CACHE_PATH . 'tmp' . DS);
 if (!is_dir(TMP_DIR)) {
 	mkdir(TMP_DIR);
