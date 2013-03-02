db2848bbc583b384a6ec1ae319d486a01253af83
?ric Ort?ga <rixo@protonmail.ch>
2013-03-02 09:44:58 +0100

    Using custom observable database session save handler.

--- php/init.inc.php
+++ php/init.inc.php
@@ -256,22 +256,21 @@ $dbAdapter = new \Zend\Db\Adapter\Adapter(array(
 	'password' => 'root',
 	'database' => 'oce_prod',
 ));
+
+use Eoze\Session\SaveHandler\ObservableDbTableGateway;
+
+// --- Session Manager
+// TableGateway
 $tableGateway = new \Zend\Db\TableGateway\TableGateway('zf_sessions', $dbAdapter);
+// Save Handler
 $saveHandlerOptions = new \Zend\Session\SaveHandler\DbTableGatewayOptions();
-// #sessionSaveHandler
-$saveHandler = new \Zend\Session\SaveHandler\DbTableGateway($tableGateway, $saveHandlerOptions);
+$saveHandler = new ObservableDbTableGateway($tableGateway, $saveHandlerOptions);;
+// SessionManager
 $sessionManager->setSaveHandler($saveHandler);
-
-//$sessionManager->setSaveHandler(new \Zend\Session\SaveHandler\Cache())
-
-//$sessionManager->start(true);
-//
-//dump($sessionManager->getId());
 
 Application::setDefaultSessionManager($sessionManager);
 Zend\Session\Container::setDefaultManager($sessionManager);
 $userSession = Application::getInstance()->getUserSession();
-$userSession->setSessionManager($sessionManager);
 
 if (ConfigManager::get('eoko/routing', 'comet', false)) {
 	$comet = new \eoko\modules\Kepler\CometEvents(MY_EOZE_PATH, $sessionManager->getId());
@@ -280,10 +279,14 @@ if (ConfigManager::get('eoko/routing', 'comet', false)) {
 	$userSession
 		->onLogin(function() use($comet, $userSession) {
 			$comet->start($userSession->getUserId());
-		})
-		->onDestroy(function() use($comet) {
-			$comet->destroy();
 		});
+
+	$saveHandler->getEventManager()->attach(
+		ObservableDbTableGateway::EVENT_DESTROY,
+		function() use($comet) {
+			$comet->destroy();
+		}
+	);
 }
 
 // Finally, start the session (must be done after the autoloader has been set,
