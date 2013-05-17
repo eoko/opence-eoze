d7e3684b01f48648e566cc814456f0e704b32d2a
eric <eric@eric-Inspiron-3721>
2013-05-17 11:39:09 +0200

    Code cleaning.

--- php/eoko/mvc/RequestReader.php
+++ php/eoko/mvc/RequestReader.php
@@ -10,15 +10,16 @@ use Zend\Mvc\Router\RouteMatch;
  *
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @author Éric Ortega <eric@planysphere.fr>
- * @since 29 juil. 2012
+ * @since 2012-07-29
  */
 interface RequestReader {
 
 	/**
-	 * 
+	 * Creates a new RequestReader.
+	 *
 	 * @param \Zend\Http\Request $request
 	 * @param \Zend\Mvc\Router\RouteMatch $routeMatch
-	 * @return \Request
+	 * @return \eoko\mvc\RequestReader
 	 */
 	function __construct(Request $request, RouteMatch $routeMatch);
 
