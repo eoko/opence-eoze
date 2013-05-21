836bf5c195442c0cbafb369d01c749d2b70bc47e
eric <eric@eric-Inspiron-3721>
2013-05-21 13:38:06 +0200

    Using HasCssFiles interface to expose Ext4 ux style resources.

--- modules/EozeExt4/EozeExt4.php
+++ modules/EozeExt4/EozeExt4.php
@@ -25,33 +25,75 @@
 namespace eoko\modules\EozeExt4;
 
 use eoko\module\Module;
+use eoko\module\traits\HasCssFiles;
+use eoko\module\traits\HasRoutes;
 
 /**
+ * Eoze module for Ext JS 4.
  *
- * @category Eoze
- * @package Ext4
- * @subpackage Module
  * @since 2012-11-28 15:57
  */
-class EozeExt4 extends Module {
+class EozeExt4 extends Module implements HasRoutes, HasCssFiles {
 
+	/**
+	 * @inheritdoc
+	 *
+	 * @internal Overridden to adds loader config for Ext4 ux extensions.
+	 */
 	public function getExt4LoaderConfig() {
 		$paths = parent::getExt4LoaderConfig();
 
+		$paths['Ext.ux'] = 'cdn/ext4ux';
+
+		return $paths;
+	}
+
+	/**
+	 * Gets the path to this module's ext4 ux source directories.
+	 *
+	 * @return string[]
+	 */
+	public function getExt4UxSourcePaths() {
+		$paths = array();
+
+		$folder = 'js.ext4-ux';
 		foreach ($this->getLocation()->getLineActualLocations(true) as $location) {
 			/** @var \eoko\module\ModuleLocation $location  */
 			$directory = $location->getDirectory();
 			$name = $location->moduleName;
 			$path = $directory->path . $name;
-			$url = $location->url;
-
-			$folder = 'js.ext4-ux';
 			if (is_dir($path . '/' . $folder)) {
-				$namespace = 'Ext.ux';
-				$paths[$namespace] = $url . $folder;
+				$paths[] = $path . '/' . $folder;
 			}
 		}
 
 		return $paths;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getRoutesConfig() {
+		$config = $this->getConfig();
+		return isset($config['router'])
+			? $config['router']
+			: null;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getModuleCssUrls() {
+		$urls = parent::getModuleCssUrls();
+
+		$files = $this->getConfig()->get('ux-css', array());
+		foreach ($files as $url) {
+			if (strstr($url, '://') === false) {
+				$url = SITE_BASE_URL . $url;
+			}
+			$urls[] = $url;
+		}
+
+		return $urls;
 	}
 }
