17ca88dcccf01e87e24f1d8fe6f38a7ffb3a293f
eric <eric@eric-Inspiron-3721>
2013-05-21 13:40:55 +0200

    Added localization for FiltersFeature ux.

---
+++ modules/EozeExt4/js.ext4/locale/fr/Ext/ux/grid/filter/DateFilter.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2013 Eoko
+ *
+ * This file is part of Opence.
+ *
+ * Opence is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Opence is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Opence. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ * @copyright Copyright (C) 2013 Eoko
+ * @licence http://www.gnu.org/licenses/gpl.txt GPLv3
+ * @author Éric Ortega <eric@eoko.fr>
+ */
+
+/**
+ * French localization for DateFilter.
+ *
+ * @since 2013-05-17 15:59
+ */
+Ext4.define('Eoze.locale.fr.Ext.ux.grid.filter.DateFilter', {
+	override: 'Ext.ux.grid.filter.DateFilter'
+
+	,afterText: "À partir du (inclus)"
+	,beforeText: "Jusqu'au (exclus)"
+	,dateFormat: "d/m/Y"
+	,onText: "Le"
+});
