dca9203da1f2fefbb476e0ec7ef61ffb1ce05f09
?ric Ort?ga <eric@planysphere.fr>
2013-08-27 12:53:23 +0200

    Changed the implementation to allow other sources to manipulate store filters, both in local or remote mode.

--- modules/EozeExt4/js.ext4/Ext/ux/grid/FiltersFeature.js
+++ modules/EozeExt4/js.ext4/Ext/ux/grid/FiltersFeature.js
@@ -1,3 +1,4 @@
+(function(Ext) {
 /**
  * Copyright (C) 2013 Eoko
  *
@@ -24,9 +25,11 @@
 /**
  * Loader for overrides of Filters feature.
  *
- * Also:
+ * Adds support for {@link Eoze.Ext.data.Types#DAYDATE} filter.
  *
- * - Adds support for {@link Eoze.Ext.data.Types#DAYDATE} filter.
+ * Replace calls to {@link Ext.data.Store#filterBy} with a unique {@link Ext.util.Filter}
+ * instance. Using `filterBy` prevents any other source from using the store's filters
+ * without interference. This approach fixes that.
  *
  * @since 2013-05-17 16:15
  */
@@ -34,9 +37,27 @@ Ext4.define('Eoze.Ext.ux.grid.FiltersFeature', {
 	override: 'Ext.ux.grid.FiltersFeature'
 
 	,requires: [
+		'Ext.util.Filter',
+		'Eoze.Ext.data.proxy.Server.FilterGetRemoteData',
 		'Eoze.Ext.ux.grid.filter.DateFilter',
 		'Eoze.Ext.ux.grid.filter.Filter.EmptyValues'
 	]
+
+	,constructor: function() {
+		var me = this;
+
+		this.callParent(arguments);
+
+		// Create unique filter
+		this.storeFilter = new Ext.util.Filter({
+			id: Ext.id()
+			,filterFn: this.getRecordFilter()
+			,property: null
+			,getRemoteData: function() {
+				return me.buildFilterData(me.getFilterData());
+			}
+		});
+	}
 
 	/**
 	 * Adds support for {@link Eoze.Ext.data.Types#DAYDATE}.
@@ -49,4 +70,68 @@ Ext4.define('Eoze.Ext.ux.grid.FiltersFeature', {
 		}
 		return this.callParent([type]);
 	}
+
+	,bindStore: function(store) {
+		var previousStore = this.store,
+			filter = this.storeFilter;
+
+		if (previousStore) {
+			previousStore.removeFilter(filter);
+		}
+
+		if (store && this.local) {
+			store.addFilter(filter);
+		}
+
+		// Parent method only binds event listeners to load methods,
+		// which have been rendered useless by the implementation
+		// changes in this override.
+
+		// this.callParent(arguments);
+	}
+
+	,reload: function() {
+		var me = this,
+			store = me.view.getStore(),
+			filter = me.storeFilter;
+		if (me.local) {
+			filter.setFilterFn(me.getRecordFilter());
+			store.filter(filter);
+		} else {
+			this.callParent(arguments);
+		}
+	}
+
+	// Rendered useless by the unique filter instance strategy.
+	,onLoad: function(store) {
+		// Intentionally empty
+	}
+
+	// Rendered useless by the unique filter instance strategy.
+	,onBeforeLoad : function (store, options) {
+		// Intentionally empty
+	}
+
+	// Builds filter data to be sent to the server.
+	,buildFilterData: function (filters) {
+		var p = {}, i, f,
+			len = filters.length,
+			data = [];
+
+		for (i = 0; i < len; i++) {
+			f = filters[i];
+			data.push(Ext.apply(
+				{},
+				// rx: adding isGridFilter tag
+				{field: f.field, isGridFilter: true},
+				f.data
+			));
+		}
+		// only build if there is active filter
+		if (data.length > 0){
+			return data;
+		}
+	}
+
 });
+})(window.Ext4 || Ext.getVersion && Ext);
