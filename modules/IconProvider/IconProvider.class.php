1cdc767688479118866bcfb7235f6e8bb1870b2e
?ric Ort?ga <eric@planysphere.fr>
2013-09-09 14:07:45 +0200

    Fixed icon provider.

--- modules/IconProvider/IconProvider.class.php
+++ modules/IconProvider/IconProvider.class.php
@@ -11,22 +11,18 @@ class IconProvider extends Module {
 
 	protected $defaultExecutor = 'css';
 
-	public static function registerEvents() {
-
-		$module = $this->getConfig()->get('HTMLPageGeneratorModule');
-		ModuleManager::getModule($module)->addListener('', $this);
-	}
-
-	public function onHtmlIncludes(HTMLPageGenerator $module) {
-		$module->pushCss(UrlMaker::getFor($this, 'getIconCss'));
-	}
-
 	public static function makeClass($file) {
 		return preg_replace(
 			'/[^a-zA-Z\d-_]/', 
 			'', 
 			preg_match('/^(.+)\.\w+$/', $file, $m) ? $m[1] : $file
 		);
+	}
+
+	public function getModuleCssUrls() {
+		return $this->isAbstract()
+			? array()
+			: array($this->getIconCssUrl());
 	}
 
 	public function listFiles() {
