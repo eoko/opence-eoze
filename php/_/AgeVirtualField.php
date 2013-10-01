78d28014570ba6c0b1a6988d6748afb8a3680c62
?ric Ort?ga <eric@planysphere.fr>
2013-10-01 07:21:46 +0200

    Extracted classes into their own file.

---
+++ php/_/AgeVirtualField.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Copyright (C) 2013 Eoko
+ *
+ * This file is part of Opence.
+ *
+ * Opence is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Opence is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Opence. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ * @copyright Copyright (C) 2013 Eoko
+ * @licence http://www.gnu.org/licenses/gpl.txt GPLv3
+ * @author Éric Ortega <eric@eoko.fr>
+ */
+
+use eoko\cqlix\Aliaser;
+
+/**
+ *
+ * @since 2013-10-02 13:01
+ */
+class AgeVirtualField extends VirtualFieldBase {
+
+	protected $dateField, $alias;
+
+	function __construct($dateField, $alias = 'age') {
+		parent::__construct($alias);
+		$this->dateField = $dateField;
+	}
+
+	public function getType() {
+		return ModelColumn::T_STRING;
+	}
+
+	public function isNullable() {
+		return true;
+	}
+
+	public function getSortClause($dir, Aliaser $aliaser) {
+		// Invert dir, because date is growing in the opposite direction compared to age
+		$dir = $dir === 'DESC' ? 'ASC' : 'DESC';
+		return parent::getSortClause($dir, $aliaser);
+	}
+
+	public function getDateField(Aliaser $aliaser) {
+		return $aliaser->alias($this->dateField);
+	}
+
+	// Age virtual field is just for display... Sorting on this field would
+	// result in sorting alphabetically, not chronologically.
+	// ... So let's sort on the real date field, instead.
+	protected function doGetSortClause(Aliaser $aliaser) {
+		return $aliaser->alias($this->dateField);
+	}
+
+	protected function doGetClause(QueryAliasable $aliasable) {
+		$dateField = $aliasable->getQualifiedName($this->dateField);
+		return <<<SQL
+(SELECT( CONVERT(CONCAT(IF((@years := (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT($dateField, '%Y')
+- (@postBD := (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT($dateField, '00-%m-%d')))
+)) > 0,CONCAT(@years,' an',IF(@years>1,'s','')),''),IF(0, '', CONCAT(
+IF((@months := FLOOR((@days := DATEDIFF(NOW(),DATE_FORMAT($dateField,
+CONCAT(YEAR(CURRENT_DATE()) - @postBD,'-%m-%d')))) / 30.4375)) >= 0
+,CONCAT(' ',@months,' mois'),''),IF(0, '', CONCAT(' '
+,(@days := FLOOR(MOD(@days, 30.4375))),CONCAT(' jour',IF(@days>0,'s',''))
+))))) USING utf8) ))
+SQL;
+//		return <<<SQL
+//(SELECT( CONVERT(CONCAT(IF((@years := (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT($dateField, '%Y')
+//- (@postBD := (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT($dateField, '00-%m-%d')))
+//)) > 0,CONCAT(@years,' an',IF(@years>1,'s','')),''),IF(@years > 18, '', CONCAT(
+//IF((@months := FLOOR((@days := DATEDIFF(NOW(),DATE_FORMAT($dateField,
+//CONCAT(YEAR(CURRENT_DATE()) - @postBD,'-%m-%d')))) / 30.4375)) >= 0
+//,CONCAT(' ',@months,' mois'),''),IF(@years >= 3, '', CONCAT(' '
+//,(@days := FLOOR(MOD(@days, 30.4375))),CONCAT(' jour',IF(@days>0,'s',''))
+//))))) USING utf8) ))
+//SQL;
+//		return <<<SQL
+//(SELECT( CONVERT(CONCAT(IF((@years := (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT($dateField, '%Y')
+//- (@postBD := (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT($dateField, '00-%m-%d')))
+//)) > 0,CONCAT(@years,' an',IF(@years>1,'s','')),''),IF(@years >= 21, '', CONCAT(
+//IF((@months := FLOOR((@days := DATEDIFF(NOW(),DATE_FORMAT($dateField,
+//CONCAT(YEAR(CURRENT_DATE()) - @postBD,'-%m-%d')))) / 30.4375)) >= 0
+//,CONCAT(IF(@year>1,' ',''),@months,' mois'),''),IF(@years >= 3, '', CONCAT(' '
+//,(@days := FLOOR(MOD(@days, 30.4375))),CONCAT(' jour',IF(@days>0,'s',''))
+//))))) USING utf8) ))
+//SQL;
+	}
+}
